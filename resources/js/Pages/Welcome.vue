67aa38fb0d7831570b6f9ee6adb203e191b18649
Douglas Santos <hiperhavy_doug@hotmail.com>
2022-02-11 00:13:56 -0300

    feat: setup inertia

---
+++ resources/js/Pages/Welcome.vue
@@ -0,0 +1,14 @@
+<template>
+    <h1>Hello</h1>
+</template>
+
+<script>
+export default {
+    props: {
+        foo: String,
+    },
+    setup(props) {
+        console.log(props)
+    }
+}
+</script>
